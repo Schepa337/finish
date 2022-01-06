5b909eb379471d56a80d280835dcfe17c4a49e01
Schepa337 <katyaoschepkova@yandex.ru>
2022-01-06 13:40:53 +0500

    forrest
    forrest

---
+++ __MACOSX/finish/fonts/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div class="block">
+				<img class="logo" src="/images/logo.png" alt="forrest">
+			</div>
+		</div>
+	</header>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,351 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Forrest - школа спортивного совершенствования</title>
+	<link rel="stylesheet" href="fonts/stylesheet.css">
+	<link rel="stylesheet" href="fonts/proxima_nova/stylesheet.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:600|Roboto:300,400,500,700&display=swap&subset=cyrillic,cyrillic-ext" rel="stylesheet">
+	<link rel="stylesheet" href="main.css">
+	<link href="js/owl/assets/owl.carousel.min.css" rel="stylesheet">
+	<link href="js/owl/assets/owl.theme.default.min.css" rel="stylesheet">
+	<link href="js/fancybox/jquery.fancybox.min.css" rel="stylesheet">
+	<meta property="og:description" content="Школа спортивного совершествования Forrest" />
+	<meta name="description" content="Школа спортивного совершествования Forrest" />
+	<meta name="mrc__share_title" content="Forrest" />
+	<meta name="mrc__share_description" content="Школа спортивного совершествования Forrest" />
+	<meta property="og:title" content="Forrest" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="" />
+	<meta property="og:site_name" content="Forrest" />
+	<meta property="og:locale" content="ru" />
+	<meta property="og:image" content="images/logo.png" />
+	<meta property="og:image:alt" content="Forrest" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="manifest" href="manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+</head>
+<body>
+	<header>
+		<div class="container-header">
+			<div class="left">
+				<a href="/" class="plashka">
+					<img class="logo" src="images/forrest_logo.png" alt="forrest">
+				</a>
+			</div>
+			<div class="mid">
+				<div class="icon-wrap">
+					<a class="icon icon-vk" target="_blank" href="https://vk.com/forrest_run_team">
+						<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m19.915 13.028c-.388-.49-.277-.708 0-1.146.005-.005 3.208-4.431 3.538-5.932l.002-.001c.164-.547 0-.949-.793-.949h-2.624c-.668 0-.976.345-1.141.731 0 0-1.336 3.198-3.226 5.271-.61.599-.892.791-1.225.791-.164 0-.419-.192-.419-.739v-5.105c0-.656-.187-.949-.74-.949h-4.126c-.419 0-.668.306-.668.591 0 .622.945.765 1.043 2.515v3.797c0 .832-.151.985-.486.985-.892 0-3.057-3.211-4.34-6.886-.259-.713-.512-1.001-1.185-1.001h-2.625c-.749 0-.9.345-.9.731 0 .682.892 4.073 4.148 8.553 2.17 3.058 5.226 4.715 8.006 4.715 1.671 0 1.875-.368 1.875-1.001 0-2.922-.151-3.198.686-3.198.388 0 1.056.192 2.616 1.667 1.783 1.749 2.076 2.532 3.074 2.532h2.624c.748 0 1.127-.368.909-1.094-.499-1.527-3.871-4.668-4.023-4.878z"/></svg>
+					</a>
+					<a class="icon icon-instagram" target="_blank" href="https://www.instagram.com/forrest_run_team/?igshid=149zva55p60zm">
+						<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m437 0h-362c-41.351562 0-75 33.648438-75 75v362c0 41.351562 33.648438 75 75 75h362c41.351562 0 75-33.648438 75-75v-362c0-41.351562-33.648438-75-75-75zm-180 390c-74.441406 0-135-60.558594-135-135s60.558594-135 135-135 135 60.558594 135 135-60.558594 135-135 135zm150-240c-24.8125 0-45-20.1875-45-45s20.1875-45 45-45 45 20.1875 45 45-20.1875 45-45 45zm0 0"/><path d="m407 90c-8.277344 0-15 6.722656-15 15s6.722656 15 15 15 15-6.722656 15-15-6.722656-15-15-15zm0 0"/><path d="m257 150c-57.890625 0-105 47.109375-105 105s47.109375 105 105 105 105-47.109375 105-105-47.109375-105-105-105zm0 0"/></svg>
+					</a>
+				</div>
+			</div>
+			<div class="right">
+				<div class="right-wrap">
+					<div class="top">
+						<a class="btn-nice" target="_blank" href="https://api.whatsapp.com/send?phone=+79125812622">Записаться</a>
+						<a class="btn-nice btn-nice_pink" target="_blank" href="https://api.whatsapp.com/send?phone=+79125812622">Консультация</a>
+					</div>
+					<div class="bottom">
+						<a href="#programs">Программы</a>
+						<a href="#approach">Наш подход</a>
+						<a href="#">Наши партнёры</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container-main">
+			<div class="main">
+				<div class="left">
+					<h1 class="h1 title">Школа спортивного совершенствования forrest</h1>
+					<div class="h4 desc">
+					В школе правильного бега вы научитесь преодолевать большие дистанции, не уставая, и подготовитесь к самым<strong> крутым забегам в мире</strong>
+				</div>
+				<div class="btn-gradient">
+					<a class="btn-color" href="#programs">Выбрать программу</a>
+				</div>
+			</div>
+			<div class="right">
+				<div class="rectangle">
+					<div id="play" class="h1 play"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
+<section id="programs" class="programs">
+	<div class="h1 title">Программы</div>
+	<div class="line"></div>
+	<div class="block">
+		<a target="_blank" href="https://api.whatsapp.com/send?phone=+79125812622" class="module module_1">
+			<div class="school">
+				<img class="img" src="images/school.png">
+			</div>
+			<div class="text">Школа</div>
+		</a>
+		<a target="_blank" href="https://api.whatsapp.com/send?phone=+79125812622" class="module module_2">
+			<div class="target">
+				<img class="img" src="images/target.png">
+			</div>
+			<div class="text">Цель</div>
+		</a>
+		<a target="_blank" href="https://api.whatsapp.com/send?phone=+79125812622" class="module module_3">
+			<div class="user">
+				<img class="img" src="images/user.png">
+			</div>
+			<div class="text">Абонемент</div>
+		</a>
+		<a target="_blank" href="https://api.whatsapp.com/send?phone=+79125812622" class="module module_4">
+			<div class="camp">
+				<img class="img" src="images/camp.png">
+			</div>
+			<div class="text">Кемп</div>
+		</a>
+		<a target="_blank" href="https://api.whatsapp.com/send?phone=+79125812622" class="module module_5">
+			<div class="phone">
+				<img class="img" src="images/phone.png">
+			</div>
+			<div class="text">Онлайн</div>
+		</a>
+		<a target="_blank" href="https://api.whatsapp.com/send?phone=+79125812622" class="module module_6">
+			<div class="event">
+				<img class="img" src="images/event.png">
+			</div>
+			<div class="text">Мероприятие</div>
+		</a>
+	</div>
+</section>
+<section id="gallary" class="gallary">
+	<div class="h1 title">Фотогалерея</div>
+	<div class="line"></div>
+	<div class="photo">
+		<div class="album">
+			<div class="picture">
+				<div class="control control-left"></div>
+			</div>
+			<div class="slider owl-carousel owl-theme" id="slider">
+				<a data-fancybox="gallery" href="images/photo_2.png" class="item screen">
+					<img class="screen" src="images/photo_2.png">
+				</a>
+				<a data-fancybox="gallery" href="images/photo_3.png" class="item screen">
+					<img class="screen" src="images/photo_3.png">
+				</a>
+				<a data-fancybox="gallery" href="images/photo_4.png" class="item screen">
+					<img class="screen" src="images/photo_4.png">
+				</a>
+				<a data-fancybox="gallery" href="images/photo_2.png" class="item screen">
+					<img class="screen" src="images/photo_2.png">
+				</a>
+				<a data-fancybox="gallery" href="images/photo_3.png" class="item screen">
+					<img class="screen" src="images/photo_3.png">
+				</a>
+				<a data-fancybox="gallery" href="images/photo_4.png" class="item screen">
+					<img class="screen" src="images/photo_4.png">
+				</a>
+				<a data-fancybox="gallery" href="images/photo_2.png" class="item screen">
+					<img class="screen" src="images/photo_2.png">
+				</a>
+				<a data-fancybox="gallery" href="images/photo_3.png" class="item screen">
+					<img class="screen" src="images/photo_3.png">
+				</a>
+				<a data-fancybox="gallery" href="images/photo_4.png" class="item screen">
+					<img class="screen" src="images/photo_4.png">
+				</a>
+			</div>
+			<div class="picture">
+				<div class="control control-right"></div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="approach" class="approach">
+	<div class="h1 title">Наш подход</div>
+	<div class="line"></div>
+	<div class="communication">
+		<div class="info info_target">
+			<div class="text">Цель</div>
+			<p>В каждой программе подготовки мы предлагаем ясную цель — финишировать на конкретном забеге. Эту цель легко представить, а результат можно измерить в часах и минутах, поэтому так легче мотивировать себя на тренировки.</p>
+		</div>
+		<div class="info info_metodika">
+			<div class="text">Методика</div>
+			<p>Тренировки построены на показателях пульса, оптимальных для каждого ученика. Вне зависимости от уровня физической подготовки вы будете тренироваться безопасно, эффективно и не будете перегружаться.</p>
+		</div>
+		<div class="info info_supertrainer">
+			<div class="text">Супертренеры</div>
+			<p>Тренеры школы — мастера спорта по легкой атлетике, многократные финишеры и призеры чемпионатов России и марафонов по всему миру.</p>
+		</div>
+		<div class="info info_team">
+			<div class="text">Команда</div>
+			<p>Новые знакомства и ценные связи — причина, по которой многие начинают заниматься спортом. Совместные впечатления и переживания на старте укрепляют любые отношения.</p>
+		</div>
+		<div class="info info_support">
+			<div class="text">Медицинская поддержка</div>
+			<p>В начале программы подготовки к цели спортивный врач посещает одну из тренировок в группе, чтобы провести экспресс-анализ опорно-двигательного аппарата под нагрузкой. Это позволяет уберечь бегунов от травм и задать вопросы специалисту.</p>
+		</div>
+		<div class="info info_headquarters">
+			<div class="text">Поддержка штаба</div>
+			<p>Мы знаем, что в процессе обучения у вас возникнет много вопросов, поэтому сделали несколько каналов связи — пользуйтесь удобным, чтобы быстро находить решение.</p>
+		</div>		
+	</div>
+	<div class="btn-exercise">
+		<a class="btn-workout" target="_blank" href="https://api.whatsapp.com/send?phone=+79125812622">Запишись на пробную тренировку бесплатно</a>
+	</div>
+</section>
+<section id="directions" class="directions">
+	<div class="h1 title">Другие направления</div>
+	<div class="line"></div>
+	<div class="tiles">
+		<div class="chips">
+			<div class="chip chip_run">
+				<div class="text">Бег</div>
+				<div class="numbers">01</div>
+			</div>
+			<div class="chip chip_physical">
+				<div class="text">Общая физическая подготовка</div>
+				<div class="numbers">02</div>
+			</div>
+			<div class="chip chip_gto">
+				<div class="text">Подготовка к ГТО</div>
+				<div class="numbers">03</div>
+			</div>
+			<div class="chip chip_stretching">
+				<div class="text">Стретчинг</div>
+				<div class="numbers">04</div>
+			</div>
+			<div class="chip chip_vuz">
+				<div class="text">Подготовка к вступительным испытаниям в ВУЗе</div>
+				<div class="numbers">05</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="marathon" class="marathon">
+	<div class="online">
+		<div class="h1 title">Онлайн подготовка планов для марафонов для полумарафонов и других дистанций</div>
+		<div class="line"></div>
+	</div>
+	<div class="btn-chat">
+		<a class="btn-chatroom" target="_blank" href="https://api.whatsapp.com/send?phone=+79125812622">Перейти в чат</a>
+	</div>
+</section>
+<section id="creator" class="creator">
+	<div class="h1 title">О создателе</div>
+	<div class="line"></div>
+	<div class="container">
+		<div class="left">
+			<div class="photo">
+				<img class="foto" src="images/foto.png">
+				<div class="name">Андрей Менякин</div>
+			</div>
+		</div>
+		<div class="right">
+			<ul>
+				<li>- Мастер спорта РФ</li>
+				<li>- Член сборной Пермского края</li>
+				<li>- Многократный чемпион Пермского края</li>
+				<li>- Многократный чемпион ПФО, УРФО</li>
+				<li>- Чемпион Российского студенческого спортивного союза(Буревестник)</li>
+				<li>- Призер Всероссийской Универсиады</li>
+				<li>- Рекордсмен города Ярославля в беге на 2000 м с/п</li>
+			</ul>
+		</div>
+	</div>
+</section>
+<section id="cost" class="cost">
+	<div class="h1 title">Стоимость</div>
+	<div class="line"></div>
+	<div class="sport">
+		<div class="sportsman_text">
+			<img class="sportsman" src="images/team.png">
+			<div class="text">Групповая тренировка</div>
+			<div class="price">3990руб/мес</div>
+		</div>
+		<div class="sportsman_text">
+			<img class="sportsman" src="images/personal.png">
+			<div class="text">Персональная тренировка</div>
+			<div class="price">500 руб</div>
+		</div>
+		<div class="sportsman_text">
+			<img class="sportsman" src="images/workout.png">
+			<div class="text">Онлайн тренировка</div>
+			<div class="price">2000 руб/мес</div>
+		</div>
+	</div>
+	<a class="btn-free" target="_blank" href="https://api.whatsapp.com/send?phone=+79125812622">Записаться на бесплатную тренировку</a>
+</section>
+<footer>
+	<div class="container-footer">
+		<div class="left">
+			<div class="logo_footer">
+				<img class="logo" src="images/forrest_logo.png" alt="forrest">
+			</div>
+		</div>
+		<div class="middle">
+			<a class="btn-nice btn-pink" target="_blank" href="https://api.whatsapp.com/send?phone=+79125812622">Записаться на пробную тренировку бесплатно</a>
+			<a class="btn-nice btn-purple" target="_blank" href="https://api.whatsapp.com/send?phone=+79125812622">Получить консультацию</a>
+		</div>
+		<div class="right">
+			<div class="icon-wrap">
+				<a class="icon icon-vk" target="_blank" href="https://vk.com/forrest_run_team">
+					<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						<path d="m19.915 13.028c-.388-.49-.277-.708 0-1.146.005-.005 3.208-4.431 3.538-5.932l.002-.001c.164-.547 0-.949-.793-.949h-2.624c-.668 0-.976.345-1.141.731 0 0-1.336 3.198-3.226 5.271-.61.599-.892.791-1.225.791-.164 0-.419-.192-.419-.739v-5.105c0-.656-.187-.949-.74-.949h-4.126c-.419 0-.668.306-.668.591 0 .622.945.765 1.043 2.515v3.797c0 .832-.151.985-.486.985-.892 0-3.057-3.211-4.34-6.886-.259-.713-.512-1.001-1.185-1.001h-2.625c-.749 0-.9.345-.9.731 0 .682.892 4.073 4.148 8.553 2.17 3.058 5.226 4.715 8.006 4.715 1.671 0 1.875-.368 1.875-1.001 0-2.922-.151-3.198.686-3.198.388 0 1.056.192 2.616 1.667 1.783 1.749 2.076 2.532 3.074 2.532h2.624c.748 0 1.127-.368.909-1.094-.499-1.527-3.871-4.668-4.023-4.878z"/></svg>
+				</a>
+				<a class="icon icon-instagram" target="_blank" href="https://www.instagram.com/forrest_run_team/?igshid=149zva55p60zm">
+					<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m437 0h-362c-41.351562 0-75 33.648438-75 75v362c0 41.351562 33.648438 75 75 75h362c41.351562 0 75-33.648438 75-75v-362c0-41.351562-33.648438-75-75-75zm-180 390c-74.441406 0-135-60.558594-135-135s60.558594-135 135-135 135 60.558594 135 135-60.558594 135-135 135zm150-240c-24.8125 0-45-20.1875-45-45s20.1875-45 45-45 45 20.1875 45 45-20.1875 45-45 45zm0 0"/><path d="m407 90c-8.277344 0-15 6.722656-15 15s6.722656 15 15 15 15-6.722656 15-15-6.722656-15-15-15zm0 0"/><path d="m257 150c-57.890625 0-105 47.109375-105 105s47.109375 105 105 105 105-47.109375 105-105-47.109375-105-105-105zm0 0"/></svg>
+				</a>				
+			</div>
+			<div class="contact-wrap">
+				<a href="tel:89875654635" class="contact contact_phone">8(987) 565-46-35</a>
+				<a href="mailto:forest.run@gmail.com" class="contact contact_mail">forest.run@gmail.com</a>
+				<div class="contact contact_address">г.Пермь ул. Пушкина д.8</div>
+			</div>
+		</div>
+	</div>
+</footer>
+<a class="btn-fly" target="_blank" href="https://api.whatsapp.com/send?phone=+79125812622">Запишись на пробную тренировку</a>
+<script
+src="https://code.jquery.com/jquery-3.4.1.min.js"
+integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+crossorigin="anonymous"></script>
+
+<script src="js/owl/owl.carousel.min.js"></script>
+<script src="js/fancybox/jquery.fancybox.min.js"></script>
+
+<script>
+	(function($) {
+		let slider = $('#slider').owlCarousel({
+			loop:true,
+			nav:false,
+			dots:true,
+			margin: 0,
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:3
+				}
+			}
+		});
+		$('.picture .control-right').click(function() {
+		    slider.trigger('next.owl.carousel');
+		});
+		$('.picture .control-left').click(function() {
+		    slider.trigger('prev.owl.carousel');
+		});
+
+		$('#play').click(function() {
+			let videoEl = $(this).parent();
+			videoEl.replaceWith('<iframe class="rectangle" src="https://www.youtube.com/embed/n98Pg6bFRY8/?&autoplay=1" controls="0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+		});
+
+	})(jQuery)
+</script>
+
+</body>
+</html>
